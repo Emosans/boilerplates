cb23bbee4343fb7596707b60a240e1365c1b8326
Emosans <shreyasd1414@gmail.com>
2024-12-05 13:54:55 +0530

    boilerplate for c files

---
+++ ConsoleProjectManager/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleProjectManager
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] options = { "C File" };
+            int chosenIndex = Util.ShowOptions(options,"Choose a setup");
+
+            switch (chosenIndex)
+            {
+                case 0: CFile(); break;
+            }
+        }
+
+        static void CFile()
+        {
+            string? name = Util.FileName("Enter the file name");
+
+            string buildsPath = Path.Combine(Directory.GetCurrentDirectory(), "builds");
+            Directory.CreateDirectory(buildsPath);
+
+            string directoryPath = Path.Combine(buildsPath, name);
+            Directory.CreateDirectory(directoryPath);
+
+            File.WriteAllText(Path.Combine(directoryPath, $"{name}.c"), $"#include \"{name}.h\"");
+
+            string headerDef = name.ToUpper();
+
+            string[] headerDefines =
+            {
+                $"#ifndef {headerDef}",
+                $"#define {headerDef}\n",
+                $"#endif // {headerDef}",
+            };
+            File.WriteAllLines(Path.Combine(directoryPath, $"{name}.h"), headerDefines);
+        }
+    }
+}
---
+++ ConsoleProjectManager/Util.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProjectManager
+{
+    internal static class Util
+    {
+        public static int ShowOptions(string[] options,string message) {
+            Console.Write(message);
+            for(int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}) \t{options[i]}");
+            }
+            Console.Write("Choice ");
+            int optionIndex = Convert.ToInt16(Console.ReadLine())-1;
+            return optionIndex;
+        }
+
+        public static string FileName(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+    }
+}
